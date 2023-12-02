2cba7e236e8ffd3ec83fa9445903acc06cf75161
superMax <euclido@live.com>
2023-12-01 21:23:47 -0500

    change file sass

--- Frontend/src/components/main/main-footer.jsx
+++ Frontend/src/components/main/main-footer.jsx
@@ -1,13 +1,7 @@
 export default function MainFooter(){
     return(
         <>
-<<<<<<< Updated upstream
-            <div className="plus">
-                Hola
-            </div>
-=======
         <div className="foot"><h1>Jesús</h1></div>
->>>>>>> Stashed changes
         </>
     )
 }
