88c425e0451a1f5ae96476b3b1d3c49e694ddd5f
superMax <euclido@live.com>
2023-12-02 03:11:40 -0500

    commit eleven

--- Frontend/src/components/main/main-header.jsx
+++ Frontend/src/components/main/main-header.jsx
@@ -1,20 +1,41 @@
-export default function MainHeader(){
-    return(
-        <header className="header">
-            <div className="container-header">
-                <div className="logo"></div>
-                <div className="input"></div>
-            </div>
-            <div className="container-header">
-                <nav className="menu">
-                    <ul className="menu__content">
-                        <li className="menu__content__item">Inicio</li>
-                        <li className="menu__content__item">Peliculas</li>
-                        <li className="menu__content__item">Iniciar Sesion</li>
-                        <li className="menu__content__item">Registrarse</li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    )
-}
+import { useState } from 'react'; 
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'; 
+import { FaBars, FaSearch } from 'react-icons/fa'; 
+// import { FaSearch } from '@fortawesome';
+ 
+const NavigationBar = () => { 
+  const [expanded, setExpanded] = useState(false); 
+ 
+  const toggleNavbar = () => { 
+    setExpanded(!expanded); 
+  }; 
+ 
+  return ( 
+    <>
+      <Navbar className='main-header' bg="light" expand="lg">
+  <Navbar.Brand className='main-header__brand' href="#home">
+    <span>QUIB-FILMS</span>
+  </Navbar.Brand>
+  <Form className='main-header__form d-flex'  inline>
+      <Button variant="outline-success"><FaSearch /></Button>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+    </Form>
+  <Navbar.Toggle aria-controls="main-header__toggler basic-navbar-nav" onClick={toggleNavbar}>
+    <FaBars />
+  </Navbar.Toggle>
+  <Navbar.Collapse id="basic-navbar-nav" className={expanded ? 'show' : 'main-header__colapse'}>
+    <Nav className="main-header__nav mr-auto">
+    <Nav.Link href="#home">Inicio</Nav.Link> 
+          <Nav.Link href="#link">Peliculas</Nav.Link> 
+          <Nav.Link href="#about">Categorías</Nav.Link> 
+          <Nav.Link href="#about">Iniciar sesión</Nav.Link> 
+          <Nav.Link href="#about">Registrarse</Nav.Link> 
+    </Nav>
+    
+  </Navbar.Collapse>
+</Navbar>
+    </>
+  ); 
+}; 
+ 
+export default NavigationBar;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Header from '../src/components/main/main-header.jsx'
 import Footer from '../src/components/main/main-footer.jsx'
 import '../src/scss/theme.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Header/>
     <Footer/>
   </React.StrictMode>,
 )
