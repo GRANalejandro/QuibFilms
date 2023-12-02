4429757983edb4b395c04d08868c071e2f651369
GRANalejandro <lolargranalejandro@gmail.com>
2023-12-01 21:47:22 -0500

    Nine commit

--- Frontend/src/layout/main.jsx
+++ Frontend/src/layout/main.jsx
@@ -6,8 +6,87 @@ export default function Main(){
         <>
             <MainHeader/>
 
-            <main className="main container-fluid ">
-
+            <main className="main container-fluid p-0">
+                <section className="background container-fluid p-0 border">
+                    <article className="background__item"></article>
+                    <article className="background__item"></article>
+                </section>
+                <section className="movies container p-0 border">
+                    <article className="movies__content">
+                        <span className="movies__content__text">
+                            Tendencias
+                        </span>
+                        <div className="movies__content__previews">
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                        </div>
+                    </article>
+                    <article className="movies__content">
+                        <span className="movies__content__text">
+                            Las 10 peliculas Mas vistas Hoy
+                        </span>
+                        <div className="movies__content__previews">
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                        </div>
+                    </article>
+                    <article className="movies__content">
+                        <span className="movies__content__text">
+                            Las mas Buscadas
+                        </span>
+                        <div className="movies__content__previews">
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                        </div>
+                    </article>
+                    <article className="movies__content">
+                        <span className="movies__content__text">
+                            Kids
+                        </span>
+                        <div className="movies__content__previews">
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                        </div>
+                    </article>
+                    <article className="movies__content">
+                        <span className="movies__content__text">
+                            Tendencias
+                        </span>
+                        <div className="movies__content__previews">
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                            <div className="movies__content__previews__item"></div>
+                        </div>
+                    </article>
+                </section>
+                <section className="page-images container-fluid">
+                    <div className="article page-images__item"></div>
+                    <div className="article page-images__item"></div>
+                </section>
             </main>
 
             <MainFooter/>
