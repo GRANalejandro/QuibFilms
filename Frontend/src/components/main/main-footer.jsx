3ded73f7d8a33449d2d365aa4d7baa9b742299dd
superMax <euclido@live.com>
2023-12-01 21:20:05 -0500

    change sass

--- Frontend/src/components/main/main-footer.jsx
+++ Frontend/src/components/main/main-footer.jsx
@@ -1,9 +1,13 @@
 export default function MainFooter(){
     return(
         <>
+<<<<<<< Updated upstream
             <div className="plus">
                 Hola
             </div>
+=======
+        <div className="foot"><h1>Jesús</h1></div>
+>>>>>>> Stashed changes
         </>
     )
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+<<<<<<< Updated upstream
 import '../src/scss/theme.scss';
 import Footer from '../src/components/main/main-footer.jsx'
+=======
+import Footer from '../src/components/main/main-footer.jsx'
+import '../src/scss/theme.scss'
+>>>>>>> Stashed changes
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+<<<<<<< Updated upstream
     <Footer />
+=======
+    <Footer/>
+>>>>>>> Stashed changes
   </React.StrictMode>,
 )
